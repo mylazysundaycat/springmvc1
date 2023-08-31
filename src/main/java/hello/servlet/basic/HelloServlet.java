caa9d04f408a13c23bf27ab013cff76165c23df7
LAZYCAT\user <mylazysundaycat@gmail.com>
2023-08-31 20:28:59 +0900

    20230831 서블릿

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet(name="helloSevlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+
+        //header 정보에 들어감
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //body 정보에 들어감
+        response.getWriter().write("hello! "+username);
+
+    }
+}
