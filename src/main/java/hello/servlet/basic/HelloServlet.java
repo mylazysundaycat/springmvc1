b2a51ba02955855500900e64fe2fd77fc1cc9ca9
Lazy Cat <mylazysundaycat@gmail.com>
2024-01-22 15:47:22 +0900

    02. Hello서브릿

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -17,14 +17,16 @@ public class HelloServlet extends HttpServlet {
         System.out.println("request = " + request);
         System.out.println("response = " + response);
 
+        // request에서 온 정보 중에 쿼리를 뽑는 것
+        // hello?username=Kim
         String username = request.getParameter("username");
         System.out.println("username = " + username);
 
-
-        //header 정보에 들어감
+        // 응답메세지 보내기
+        // HTTP 컨텐트 header에 들어감
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-        //body 정보에 들어감
+        // HTTP body 정보에 들어감
         response.getWriter().write("hello! "+username);
 
     }
